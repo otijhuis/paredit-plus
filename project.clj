2b3e07081262d01e1971fceb4f161c70121f4d73
Okke Tijhuis <o.tijhuis@gmail.com>
2014-02-20 00:30:21 +0100

    Update version to 0.0.5

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/paredit-plus "0.0.4"
+(defproject com.lighttable/paredit-plus "0.0.5"
   :description "paredit+ plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
