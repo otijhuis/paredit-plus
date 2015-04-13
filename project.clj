9d5870335684f3532463829d87ac34fb1aac72fd
Okke Tijhuis <o.tijhuis@gmail.com>
2015-04-13 18:57:22 +0200

    Dummy version update to see if a new release fixes the zlib installation error

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/paredit-plus "0.0.6"
+(defproject com.lighttable/paredit-plus "0.0.7"
   :description "paredit+ plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
